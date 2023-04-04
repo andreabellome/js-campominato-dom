8f31a9671392b1c4e5a4a78e94e22230ae0d4bb6
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-04 20:12:50 +0200

    creazione campo minato

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,11 @@ function createGrid(){
     }
     diffValue = parseInt(diffValue);
 
+    /* create cells with bombs */
+    let bombsArray = randArray(1, diffValue, 16);
+
+    console.log(bombsArray)
+
     /* refresh */
     main.innerHTML = '';
 
@@ -33,7 +38,7 @@ function createGrid(){
     main.append(divGrid)
 
     /* create the cell num */
-    myFunction_set( diffValue )
+    setRootProp( diffValue )
 
     /* create items in the grid */
     for (let i = 1; i <= diffValue; i++){
@@ -44,11 +49,27 @@ function createGrid(){
         divItem.innerText = i;
         document.querySelector('.grid').append(divItem);
 
-        /* al click, colora la cella e prendi il valore */
-        divItem.addEventListener('click', function(){
+        /* al click, controlla se utente ha clickato una bomba */
+        check = checkValueArray(i, bombsArray);
+        if (check == 1){
+
+            /* al click, colora la cella e prendi il valore */
+            divItem.addEventListener('click', function(){
+            console.log(this.innerText);
+            this.classList.add('bomb');
+            })
+
+        } else {
+
+            /* al click, colora la cella e prendi il valore */
+            divItem.addEventListener('click', function(){
             console.log(this.innerText);
             this.classList.add('clicked');
-        })
+            })
+
+        }
+
+        
 
     }
 
@@ -56,10 +77,45 @@ function createGrid(){
 /* end: function to create the grid */
 
 /* start: create a function for setting a variable value */
-function myFunction_set( x ) {
+function setRootProp( x ) {
     x = Math.sqrt( x );
     r.style.setProperty('--cellNum', x); /* set the value */
 }
 /* end: create a function for setting a variable value */
 
+/* start: create an array of different random numbers */
+function randArray(min, max, dim){
+
+    let randArr = [];
+    if (dim > max){
+        console.log( 'dimension of the array should be less than max. value' ); 
+    } else {
+        for (let i = 0; i < dim; i++){
+            let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            while (randArr.includes(randomNumber)) {
+                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            randArr.push(randomNumber);
+        }
+    }
+
+    return randArr;
+
+}
+/* end: create an array of different random numbers */
+
+/* start: check if a value is in array */
+function checkValueArray(val, arr){
+
+    if ( arr.indexOf(val) !== -1 ){
+        var check = 1; /* value is in array */
+    } else {
+        var check = 0; /* value is not in array */
+    }
+
+    return check;
+
+}
+
+/* end: check if a value is in array */
 
